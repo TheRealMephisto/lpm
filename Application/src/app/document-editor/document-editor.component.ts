bd294f012c4383cc072dd0dd691a9721bd4d676d
TheRealMephisto <max@wechsel-wissen.de>
2020-03-08 20:11:08 +0100

    added forms for title, path, author

--- Application/src/app/document-editor/document-editor.component.ts
+++ Application/src/app/document-editor/document-editor.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
+/**
+ * Provide a form enabling the user to edit database entries, i.e. of TeX-Documents.
+ */
 @Component({
   selector: 'app-document-editor',
   templateUrl: './document-editor.component.html',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentEditorComponent implements OnInit {
 
+  fileEnding: string = 'tex'
+
   constructor() { }
 
   ngOnInit() {
+    this.author.setValue('devUser')
+  }
+
+  title = new FormControl('', [Validators.required]);
+  path = new FormControl('', [Validators.required, Validators.pattern('.*\.'.concat(this.fileEnding))]);
+  author = new FormControl('', [Validators.required]);
+
+  getErrorMessage(target: string): string {
+    let targetForm: FormControl;
+    switch(target) {
+      case 'title':
+        targetForm = this.title;
+        break;
+      case 'path':
+        targetForm = this.path;
+        break;
+      case 'author':
+        targetForm = this.author;
+        break;
+      default:
+        return;
+    }
+    if (targetForm.hasError('required')) {
+      return 'You must enter a value';
+    }
+    if (targetForm.hasError('pattern')) {
+      return 'Must be a file path ending on .'.concat(this.fileEnding);
+    }
   }
 
 }
