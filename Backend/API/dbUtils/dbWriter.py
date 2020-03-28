92d2b72f856bd81a8274a6bf59b065271e4648f2
TheRealMephisto <max@wechsel-wissen.de>
2020-03-28 10:34:06 +0100

    improved readability and fixed some errors

--- Backend/API/dbUtils/dbWriter.py
+++ Backend/API/dbUtils/dbWriter.py
@@ -95,39 +95,73 @@ class dbWriter:
     def addTexDocumentEntry(self, title, path, username, filePathList, informationList, informationTypeList, packageList, packageOptionsList):
 
         # ensure user in table and get id
-        dataDictToAdd = {'username': username}
-        self.userId = self.ensureEntryInTable(dataDictToAdd, 'users')
+        self.userId = self.ensureEntryInTable(
+                            {
+                                'username': username
+                            },
+                            'users'
+                        )
 
         # Add content
-        dataDictToAdd = {'title' : title, 'path' : path}
-        contentId = self.ensureEntryInTable(dataDictToAdd, 'contents')
+        contentId = self.ensureEntryInTable(
+                            {
+                                'title' : title,
+                                'path' : path
+                            },
+                            'contents'
+                        )
 
         # Add information type and information if not already existing
         informationTypeIds = list()
         informationIds = list()
+
         creationDateProvided = False
         if len(informationTypeList) != len(informationList):
             self.procedureProtocol['informationType'] = 'Error! informationList and informationTypeList must be of same length!'
             self.procedureProtocol['information'] = 'Error! informationList and informationTypeList must be of same length!'
+
         for i in range(0, len(informationTypeList)):
             if informationTypeList[i] == 'creationDate':
                 creationDateProvided = True
+
         if not creationDateProvided:
-            dataDictToAdd= {'type' : 'creationDate'}
-            entryId = self.ensureEntryInTable(dataDictToAdd, 'informationType')
-            informationTypeIds.append(entryId)
+            informationTypeIds.append(
+                self.ensureEntryInTable(
+                    {
+                        'type' : 'creationDate'
+                    },
+                    'informationType'
+                )
+            )
 
-            dataDictToAdd = {'information' : self.getCurrentSqlTimestamp(), 'informationTypeId' : entryId}
-            entryId = self.ensureEntryInTable(dataDictToAdd, 'information')
-            informationIds.append(entryId)
+            informationIds.append(
+                self.ensureEntryInTable(
+                    {
+                        'information' : self.getCurrentSqlTimestamp(),
+                        'informationTypeId' : entryId
+                    },
+                    'information'
+                )
+            )
         for i in range(0 if creationDateProvided else 1, len(informationTypeList)):
-            dataDictToAdd = {'type' : informationTypeList[i]}
-            entryId = self.ensureEntryInTable(dataDictToAdd, 'informationType')
-            informationTypeIds.append(entryId)
+            informationTypeIds.append(
+                self.ensureEntryInTable(
+                    {
+                        'type' : informationTypeList[i]
+                    },
+                    'informationType'
+                )
+            )
 
-            dataDictToAdd = {'information' : informationList[i], 'informationTypeId' : entryId}
-            entryId = self.ensureEntryInTable(dataDictToAdd, 'information')
-            informationIds.append(entryId)
+            informationIds.append(
+                self.ensureEntryInTable(
+                    {
+                        'information' : informationList[i],
+                        'informationTypeId' : entryId
+                    },
+                    'information'
+                )
+            )
 
         # Add package and options combination if not already existing
         packageIds = list()
@@ -137,45 +171,90 @@ class dbWriter:
             self.procedureProtocol['packages'] = 'Error! packageList and packageOptionsList must be of same length!'
         else:
             for i in range(0, len(packageList)):
-                self.procedureProtocol['packageRoption'][str(i)] = dict()
-                dataDictToAdd = {'package' : packageList[i]}
-                entryId = self.ensureEntryInTable(dataDictToAdd, 'packages' [str(i)])
-                packageIds.append(entryId)
+                packageIds.append(
+                    self.ensureEntryInTable(
+                        {
+                            'package' : packageList[i]
+                        },
+                        'packages',
+                        [str(i)]
+                    )
+                )
 
                 packageOptionIds.append(list())
 
                 for j in range(0, len(packageOptionsList[i])):
-                    dataDictToAdd = {'option' : packageOptionsList[i][j]}
-                    entryId = self.ensureEntryInTable(dataDictToAdd, 'packageOptions', [str(i)])
-                    packageOptionIds[i].append(entryId)
+                    packageOptionIds[i].append(
+                        self.ensureEntryInTable(
+                            {
+                                'option' : packageOptionsList[i][j]
+                            },
+                            'packageOptions',
+                            [str(i)]
+                        )
+                    )
 
                     # Add relation from package to package option
-                    dataDictToAdd = {'packageId' : packageIds[i], 'optionId' : insertionOutput['entryId']}
-                    self.ensureEntryInTable(dataDictToAdd, 'packageRoption', [str(i), str(j)])
+                    self.ensureEntryInTable(
+                        {
+                            'packageId' : packageIds[i],
+                            'optionId' : insertionOutput['entryId']
+                        },
+                        'packageRoption',
+                        [str(i), str(j)]
+                    )
 
         # Add file paths if not already existing
         fileIds = list()
         for i in range(0, len(filePathList)):
-            dataDictToAdd = {'path' : filePathList[i]}
-            entryId = self.ensureEntryInTable(dataDictToAdd, 'files', ['files', str(i)])
-            fileIds.append(entryId)
+            fileIds.append(
+                self.ensureEntryInTable(
+                    {
+                        'path' : filePathList[i]
+                    },
+                    'files',
+                    [str(i)]
+                )
+            )
 
         # Add contentRuser, contentRfile, contentRinformation and contentRpackages entry
-        dataDictToAdd = {'contentId' : contentId, 'userId' : self.userId}
-        self.ensureEntryInTable(dataDictToAdd, 'contentRuser')
+        self.ensureEntryInTable(
+            {
+                'contentId' : contentId,
+                'userId' : self.userId
+            },
+            'contentRuser'
+        )
 
         for i in range(0, len(fileIds)):
-            dataDictToAdd = {'contentId' : contentId, 'fileId' : fileIds[i]}
-            self.ensureEntryInTable(dataDictToAdd, 'contentRfile', [str(i)])
+            self.ensureEntryInTable(
+                {
+                    'contentId' : contentId,
+                    'fileId' : fileIds[i]
+                },
+                'contentRfile',
+                [str(i)]
+            )
         
         for i in range(0, len(informationIds)):
-            dataDictToAdd = {'contentId' : contentId, 'informationId' : informationIds[i]}
-            self.ensureEntryInTable(dataDictToAdd, 'contentRinformation', [str(i)])
+            self.ensureEntryInTable(
+                {
+                    'contentId' : contentId,
+                    'informationId' : informationIds[i]
+                },
+                'contentRinformation',
+                [str(i)]
+            )
 
         for i in range(0, len(packageIds)):
-            dataDictToAdd = {'contentId' : contentId, 'packageId' : packageIds[i]}
-            with dbReader() as db_reader:
-                self.ensureEntryInTable(dataDictToAdd, 'contentRpackage', [str(i)])
+            self.ensureEntryInTable(
+                {
+                    'contentId' : contentId,
+                    'packageId' : packageIds[i]
+                },
+                'contentRpackage',
+                [str(i)]
+            )
 
         self.dbConnection.commit()
 
